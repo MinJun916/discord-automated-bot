659b83125ba75967ac9bb81303d1bc83baeeae86
MinJun916 <ed125248@gmail.com>
2025-12-05 18:05:05 +0900

    chore: update bot cron schedule

--- src/bot/botSchedule.ts
+++ src/bot/botSchedule.ts
@@ -2,7 +2,7 @@ export const dailyMorning = '0 9 * * 1-5';
 
 export const dailyNight = '0 19 * * 1-4';
 
-export const weeklyNight = '0 19 * * 5';
+export const weeklyNight = '0 18 * * 5';
 
 export const dailyHardManagement = '0 10,14,16,18 * * 1-5';
 
